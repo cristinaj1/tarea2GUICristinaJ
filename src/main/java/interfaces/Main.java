3ee76f34d8d5f2596273d3fdbbc676f0e953fe6e
cristinaj1 <cristinacee305@gmail.com>
2021-03-23 15:54:04 +0100

    Mejorado y separado

---
+++ src/main/java/interfaces/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Cris
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Construimos la ventana
+        JFrame frame = new JFrame("App");
+        // La ventana no se puede redimensionar
+        frame.setResizable(false);
+        // Posición de la ventana
+        frame.setLocationRelativeTo(null);
+        // Incluimos el panel en la ventana
+        frame.add(new Saludo());
+        // Ajusta el frame al contenido
+        frame.pack();
+        // Hacemos visible la ventana
+        frame.setVisible(true);
+        //Acción por defecto al pulsar el botón de cierre de la ventana
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    
+}
